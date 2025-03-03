04266476104e1fd9c389d054e241ec4ea5197dee
itspaudal-git <80719235+itspaudal-git@users.noreply.github.com>
2025-03-03 04:29:04 -0600

    Update credd.js

--- credd.js
+++ credd.js
@@ -1,14 +1,4 @@
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAD1zPEv79iJvmP55zbwgtKMdKXOh77u0E",
-  authDomain: "music-427615.firebaseapp.com",
-  databaseURL: "https://music-427615-default-rtdb.firebaseio.com",
-  projectId: "music-427615",
-  storageBucket: "music-427615.appspot.com",
-  messagingSenderId: "671032405973",
-  appId: "1:671032405973:web:79800ac33022aff2a3975c",
-  measurementId: "G-932Y8LTJ7Y"
-};
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
